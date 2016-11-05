96d16a3661e7dfe35887ee07b84c315e666bbcfa
Pascal Kleindienst <mail@pascalkleindienst.de>
2016-11-05 14:22:10 +0100

    added main Plugin file

---
+++ Plugin.php
@@ -0,0 +1,50 @@
+<?php namespace PKleindienst\GithubProjects;
+
+use System\Classes\PluginBase;
+
+class Plugin extends PluginBase
+{
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'Github Projects',
+            'description' => '',
+            'author'      => 'Pascal Kleindienst',
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function registerComponents()
+    {
+        return [
+            'PKleindienst\GithubProjects\Components\Item' => 'ghItem',
+        //     'PKleindienst\GithubProjects\Components\List' => 'ghList'
+        ];
+    }
+
+    /**
+     * Register new Twig variables
+     * @return array
+     */
+    public function registerMarkupTags()
+    {
+        // Check the translate plugin is installed
+        if (class_exists('RainLab\Translate\Behaviors\TranslatableModel')) {
+            return [];
+        }
+
+        return [
+            'filters' => [
+                '_'  => ['Lang', 'get'],
+                '__' => ['Lang', 'choice'],
+            ]
+        ];
+    }
+}
