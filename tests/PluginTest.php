6889c1644a31cfe8482d8835173a8ed6c09e917f
Pascal Kleindienst <mail@pascalkleindienst.de>
2017-04-21 20:05:14 +0200

    added plugin test

---
+++ tests/PluginTest.php
@@ -0,0 +1,50 @@
+<?php namespace PKleindienst\GithubProjects\Tests;
+
+use PKleindienst\GithubProjects\Plugin;
+use PluginTestCase;
+
+class PluginTest extends PluginTestCase
+{
+    protected $plugin;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->plugin = new Plugin($this->app);
+    }
+
+    public function testPluginDetails()
+    {
+        $details = $this->plugin->pluginDetails();
+        $this->assertTrue(is_array($details));
+        $this->assertCount(3, $details);
+    }
+
+    /**
+     * @dependsOn testPluginDetails
+     */
+    public function testPluginDetailsKeys()
+    {
+        $details = $this->plugin->pluginDetails();
+        $this->assertArrayHasKey('name', $details);
+        $this->assertArrayHasKey('description', $details);
+        $this->assertArrayHasKey('author', $details);
+    }
+
+    public function testRegisterComponents()
+    {
+        $components = $this->plugin->registerComponents();
+        $this->assertTrue(is_array($components));
+        $this->assertArrayHasKey('PKleindienst\GithubProjects\Components\Item', $components);
+        $this->assertArrayHasKey('PKleindienst\GithubProjects\Components\ProjectList', $components);
+    }
+    
+    public function testRegisterMarkupTagsNotEmpty()
+    {
+        $tags = $this->plugin->registerMarkupTags();
+        $this->assertTrue(is_array($tags));
+        $this->assertArrayHasKey('filters', $tags);
+        $this->assertArrayHasKey('_', $tags['filters']);
+        $this->assertArrayHasKey('__', $tags['filters']);
+    }
+}
