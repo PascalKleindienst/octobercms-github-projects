286603669430f83bcac13c06e5e36ba302a7f18a
Pascal Kleindienst <mail@pascalkleindienst.de>
2017-04-23 11:07:05 +0200

    added en translation

---
+++ lang/en/lang.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+    'plugin' => [
+        'name'        => 'Github Projects',
+        'description' => 'Utilize the GitHub API to show informations about repositories.'
+    ],
+    'item' => [
+        'name'        => 'Github Repository Item',
+        'description' => 'Displays information about a GitHub repository.',
+        'user_title'  => 'User',
+        'user_desc'   => 'The user whose repository is displayed',
+        'repo_title'  => 'Repository',
+        'repo_desc'   => 'The repository name'
+    ],
+    'list' => [
+        'name'               => 'Github Repository List',
+        'description'        => 'List public repositories for the specified user.',
+        'user_title'         => 'User',
+        'user_desc'          => 'The user whose repositories are displayed',
+        'type_title'         => 'Type',
+        'type_desc'          => 'The repository type. Can be one of all, owner, member',
+        'type_opt_all'       => 'All',
+        'type_opt_owner'     => 'Owner',
+        'type_opt_member'    => 'Member',
+        'sort_title'         => 'Sorting',
+        'sort_desc'          => 'The sorting field. Can be one of created, updated, pushed, full_name',
+        'sort_opt_created'   => 'Created',
+        'sort_opt_updated'   => 'Updated',
+        'sort_opt_pushed'    => 'Pushed',
+        'sort_opt_fullname'  => 'Full-Name',
+        'direction_title'    => 'Sort Direction',
+        'direction_desc'     => 'The sort direction. Can be one of asc or desc',
+        'direction_opt_asc'  => 'Ascending',
+        'direction_opt_desc' => 'Descending'
+    ]
+];
